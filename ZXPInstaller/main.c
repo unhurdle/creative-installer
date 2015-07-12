9b6c8d9d83b5ce61b0f19c9f692302d7d041b1d4
Kris Coppieters <kris@rorohiko.com>
2015-07-13 08:08:15 +1200

    Initial Commit

---
+++ ZXPInstaller/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ZXPInstaller
+//
+//  Created by Kris Coppieters on 13/07/15.
+//  Copyright (c) 2015 Kris Coppieters. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+    return 0;
+}
